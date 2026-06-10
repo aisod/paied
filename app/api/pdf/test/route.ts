1da250f7c150b2c38dc0df84200c75f5ff099625
AISOD Institute <aisodinstitute@gmail.com>
2026-06-10 16:33:54 +0200

    vercel

--- app/api/pdf/test/route.ts
+++ app/api/pdf/test/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import PDFDocument from 'pdfkit'
 
+export const dynamic = 'force-dynamic'
 export const runtime = 'nodejs'
 
 export async function GET(): Promise<Response> {
